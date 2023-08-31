e6df367367307a1ca1533a9b8a7bd430f2c45f49
sai kumar <117747697+SaiKumar1506@users.noreply.github.com>
2023-08-31 10:36:24 +0530

    Update exp-20.R

--- exp-20.R
+++ exp-20.R
@@ -1,6 +1,8 @@
-# Create a new empty plot without specifying axes limits
-plot.new()
-
-# Create a new empty plot with specified axes limits
-plot.new()
-plot.window(xlim = c(0, 10), ylim = c(0, 20))
+# Set up the plot area
+plot(x = NULL, y = NULL, # Empty data to create an empty plot
+     xlim = c(0, 10),    # Specify x-axis limits
+     ylim = c(0, 20),    # Specify y-axis limits
+     xlab = "X-axis",    # Label for the x-axis
+     ylab = "Y-axis",    # Label for the y-axis
+     main = "Empty Plot with Axis Limits" # Title of the plot
+)
